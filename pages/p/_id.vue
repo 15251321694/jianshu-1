872cb129264635e767f78f795814a80cbb4977ea
lizhiyuan <148334733@qq.com>
2018-04-25 14:58:21 +0800

    分享布局完成

--- pages/p/_id.vue
+++ pages/p/_id.vue
@@ -84,15 +84,15 @@
                     </div>
                     <div class="share">
                         <a href="#" class="share-btn">
-                            <i class="fa fa-weixin"></i>
+                            <i class="fa fa-qq qq"></i>
                         </a>
                         <a href="#" class="share-btn">
-                            <i class="fa fa-weibo"></i>
+                            <i class="fa fa-weibo weibo"></i>
                         </a>
                         <a href="#" class="share-btn">
-                            <i class="fa fa-weixin"></i>
+                            <i class="fa fa-weixin weixin"></i>
                         </a>
-                        <a href="#" class="more-share">
+                        <a href="#" class="share-btn more-share">
                             更多分享
                         </a>
                     </div>
